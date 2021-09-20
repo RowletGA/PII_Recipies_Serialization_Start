e4e1817c29828b36505dde7e427a802a7206131e
Joaquín Pérez <joaquin.perez@correo.ucu.edu.uy>
2021-09-20 20:48:13 -0300

    Programa corriendo , con errores en el test

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -11,14 +11,14 @@ using System.Text.Json.Serialization;
 
 namespace Recipies
 {
-    public class Recipe
+    public class Recipe : IJsonConvertible
     {
         // private ArrayList steps = new ArrayList();
         public Product FinalProduct { get; set; }
 
         [JsonInclude]
         public ArrayList Steps { get; private set; } = new ArrayList();
-
+        
 
         public void AddStep(Step step)
         {
@@ -29,5 +29,15 @@ namespace Recipies
         {
             this.Steps.Remove(step);
         }
+         public string ConvertToJson()
+        {
+            return JsonSerializer.Serialize(this.Steps);
+        }
+        public void LoadFromJson(string json)
+        {
+            Recipe deserialized = JsonSerializer.Deserialize<Recipe>(json);
+            
+        }
+
     }
 }
